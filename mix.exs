9b5dc02fad00899fa41408dd1bef602bb0e89496
Jaime Iniesta <jaimeiniesta@gmail.com>
2016-06-22 17:00:22 +0200

    release 0.0.2

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Funkspector.Mixfile do
 
   def project do
     [app: :funkspector,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.3",
      description: "Web page inspector for Elixir",
      package: package,
