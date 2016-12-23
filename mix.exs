aa3d64108b9c4538cecc9b368c5d4ed43cd387e4
Jaime Iniesta <jaimeiniesta@gmail.com>
2016-12-23 06:17:55 +0100

    update dependencies

--- mix.exs
+++ mix.exs
@@ -32,7 +32,7 @@ defmodule Funkspector.Mixfile do
     [
       { :httpoison, "~> 0.9" },
       { :floki,     "~> 0.9" },
-      { :quinn,     "~> 0.0.4" },
+      { :quinn,     "~> 1.0.0" },
       { :mock,      "~> 0.1", only: :test},
       { :ex_doc,    ">= 0.0.0", only: :dev }
     ]
